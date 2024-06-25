3851ab2b2f57e0d07ec5065af1eb34aee4c36b09
athishsb <athishbalajisb22@gmail.com>
2024-06-25 06:54:33 +0530

    updated api.js

--- src/Global/api.js
+++ src/Global/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://task-management-backend-5qx2.onrender.com/api",
 });
 export default api;
